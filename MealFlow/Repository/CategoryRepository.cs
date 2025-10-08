061897cb9594fb4e3ee2388a6d4d193a9dbf8ec2
DawidMielniczek <dawid.mielniczek99@gmail.com>
2025-10-08 20:19:45 +0200

    Create CategoryRepository

---
+++ MealFlow/Repository/CategoryRepository.cs
@@ -0,0 +1,58 @@
+﻿using MealFlow.Data;
+using MealFlow.Repository.IRepository;
+
+namespace MealFlow.Repository
+{
+    public class CategoryRepository : ICategory
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Category Create(Category category)
+        {
+            _db.Category.Add(category);
+            _db.SaveChanges();
+            return category;
+        }
+
+        public bool Delete(int id)
+        {
+            var obj = _db.Category.Where(x => x.Id == id).FirstOrDefault();
+            if(obj != null)
+            {
+                _db.Category.Remove(obj);
+                return _db.SaveChanges() >0;
+            }
+            return false;
+        }
+
+        public Category Get(int id)
+        {
+            var obj = _db.Category.Where(x => x.Id ==id).FirstOrDefault();
+            if (obj != null)
+                return new Category();
+            return obj;
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            return _db.Category.ToList();
+        }
+
+        public Category Update(Category category)
+        {
+            var obj = _db.Category.Where(x => x.Id == category.Id).FirstOrDefault();
+            if(obj is not null)
+            {
+                obj.Name = category.Name;
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return obj;
+            }
+            return new Category();
+        }
+    }
+}
---
+++ MealFlow/Repository/IRepository/ICategory.cs
@@ -0,0 +1,14 @@
+﻿using MealFlow.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealFlow.Repository.IRepository
+{
+    public interface ICategory
+    {
+        public Category Get(int id);
+        public IEnumerable<Category> GetAll();
+        public Category Create(Category category);
+        public Category Update(Category category);
+        public bool Delete(int id);
+    }
+}
