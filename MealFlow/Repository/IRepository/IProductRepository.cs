35212bf3735baf2504f0c66f9b1638d0391e5837
DawidMielniczek <dawid.mielniczek99@gmail.com>
2025-10-10 19:44:27 +0200

    Create Product repository etc

---
+++ MealFlow/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using MealFlow.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealFlow.Repository.IRepository
+{
+    public interface IProductRepository
+    {
+        public Task<Product> GetAsync(int id);
+        public Task<IEnumerable<Product>> GetAllAsync();
+        public Task<Product> CreateAsync(Product product);
+        public Task<Product> UpdateAsync(Product product);
+        public Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ MealFlow/Repository/ProductRepository.cs
@@ -0,0 +1,62 @@
+﻿using MealFlow.Data;
+using MealFlow.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace MealFlow.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            await _db.Product.AddAsync(product);
+            await _db.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var obj = await _db.Product.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if(obj != null)
+            {
+                _db.Product.Remove(obj);
+                return await _db.SaveChangesAsync() >0;
+            }
+            return false;
+        }
+
+        public async Task<Product> GetAsync(int id)
+        {
+            var obj = await _db.Product.Where(x => x.Id ==id).FirstOrDefaultAsync();
+            if (obj != null)
+                return obj;
+            return new Product();
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _db.Product.ToListAsync();
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            var obj = await _db.Product.Where(x => x.Id == product.Id).FirstOrDefaultAsync();
+            if(obj is not null)
+            {
+                obj.Name = product.Name;
+                obj.Description = product.Description;
+                obj.CategoryId = product.CategoryId;
+                obj.Price = product.Price;
+                _db.Product.Update(obj);
+                await _db.SaveChangesAsync();
+                return obj;
+            }
+            return new Product();
+        }
+    }
+}
