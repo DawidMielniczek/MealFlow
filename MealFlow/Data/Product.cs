a9bc3f90fb41573674e757c81a1103ab5d7516cf
DawidMielniczek <dawid.mielniczek99@gmail.com>
2025-10-10 19:29:29 +0200

    Add Product Model

--- MealFlow/Data/Product.cs
+++ MealFlow/Data/Product.cs
@@ -1,7 +1,22 @@
-﻿namespace MealFlow.Data
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MealFlow.Data
 {
     public class Product
     {
-        public int Id 
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(0.01, 1000)]
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string? SpecialTag { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public string? ImageUrl { get; set; }
+
+
     }
 }
