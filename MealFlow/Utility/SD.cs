8f3c52d9aec1b0c3e580a948525e366676e5257e
DawidMielniczek <dawid.mielniczek99@gmail.com>
2025-10-13 20:36:30 +0200

    Add a new navbar

---
+++ MealFlow/Utility/SD.cs
@@ -0,0 +1,8 @@
+﻿namespace MealFlow.Utility
+{
+    public static class SD
+    {
+        public static string  Role_admin = "Admin";
+        public static string  Role_Customer = "Customer";
+    }
+}
